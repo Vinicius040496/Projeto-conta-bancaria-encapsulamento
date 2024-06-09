560f88b94ef9cd8fef558d5b341ca7401a681da8
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-09 13:36:00 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp134/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp134
+{
+    internal class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome;
+        public double DepositoIni {  get; private set; }
+
+        public Conta(int numeroconta, string nome, double depositoini)
+        {
+            NumeroConta = numeroconta;
+            Nome = nome;
+            DepositoIni = depositoini;
+        }
+
+        public Conta(int numeroConta, string nome)
+        {
+            NumeroConta = numeroConta;
+            Nome = nome;
+        }
+
+        public void DepositoAd( double DepositoAdicional)
+        {
+            DepositoIni += DepositoAdicional;
+        }
+
+        public void Saque(double saque)
+        {
+            DepositoIni -= saque + 5.00;
+               
+        }
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta + " , Titular: " + Nome + " , Saldo: " + " R$"+DepositoIni.ToString("f2");
+        }
+       
+    }
+}
---
+++ ConsoleApp134/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp134
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta Conta;
+            Console.Write("Digite numero da conta: ");
+            int numeroconta = int.Parse(Console.ReadLine());
+            Console.Write("Digite titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial s/n: ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S'){
+                Console.Write("Qual valor do deposito inicial:  ");
+                double depositoini = double.Parse(Console.ReadLine());
+                Conta = new Conta(numeroconta, nome, depositoini);
+            }
+            else
+            {
+                Conta = new Conta(numeroconta, nome);
+            }
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(Conta);
+            Console.WriteLine();
+
+            Console.Write("Entre com valor de deposito: ");
+            double DepositoAdicional = double.Parse(Console.ReadLine());
+            Conta.DepositoAd(DepositoAdicional);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.Write(Conta);
+
+            Console.WriteLine();
+
+            Console.Write("Entre com valor de saque: ");
+            double saque = double.Parse(Console.ReadLine());
+            Conta.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.Write(Conta);
+            Console.WriteLine(" , Taxa de saque 5,00 reais!");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
